bcacc1e82f89ab4cb36714200c9e7bdb62284413
Emil Karlsson <own.emka@gmail.com>
2020-04-14 12:45:02 +0200

    Add base with window creation in Main

---
+++ V-Engine/WinMain.cpp
@@ -0,0 +1,63 @@
+#include <Windows.h>
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch ( msg )
+	{
+	case WM_CLOSE:
+		PostQuitMessage(69);
+		break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+
+int CALLBACK WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_ HINSTANCE hPrevInstance,
+	_In_ LPSTR lpCmdLine,
+	_In_ int nCmdShow
+)
+{
+
+	const wchar_t *pClassName = L"hw3dbutts";
+
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+	RegisterClassEx(&wc);
+
+	HWND hWnd = CreateWindowEx(
+		0, pClassName,
+		L"My window",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200, 640, 480,
+		nullptr, nullptr, hInstance, nullptr
+	);
+
+	ShowWindow(hWnd, SW_SHOW);
+
+	MSG msg;
+	BOOL gResult;
+	while ( (gResult = GetMessage(&msg, nullptr, 0, 0)) > 0 )
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if ( gResult == -1 )
+		return 0;
+	return msg.wParam;
+
+	return EXIT_SUCCESS;
+}
