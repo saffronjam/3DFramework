590d7be626e0c48c77f79fe8b4a5a7cf66df06b0
Emil Karlsson <own.emka@gmail.com>
2020-11-24 23:00:23 +0100

    Full recover and big clean up after CMake attempt

--- Saffron/Source/Implementation/EditorApplication.cpp
+++ Saffron/Source/Implementation/EditorApplication.cpp
@@ -8,45 +8,48 @@ namespace Se {
 
 Application *Se::CreateApplication()
 {
-    return new EditorApplication({ "Saffron Engine", 1700, 900 });
+	return new EditorApplication({ "Saffron Engine", 1700, 900 });
 }
 
 EditorApplication::EditorApplication(const Properties &props)
-        :
-        Application(props),
-        m_StartupLayer(Shared<StartupLayer>::Create()) {
-    const auto wantProjectSelectorFn = [this] {
-        Run::Later([=] {
-            PopLayer();
-            m_PreLoader->Reset();
-            m_EditorLayer.Reset();
-            PushLayer(m_StartupLayer);
-        });
-    };
-    const auto projectSelectFn = [wantProjectSelectorFn, this](const Shared<Project> &project) {
-        Run::Later([=] {
-            PopLayer();
-            m_PreLoader->Reset();
-            m_EditorLayer = Shared<EditorLayer>::Create(project);
-            m_EditorLayer->GetSignal(EditorLayer::Signals::OnWantProjectSelector).Connect(wantProjectSelectorFn);
-            PushLayer(m_EditorLayer);
-        });
-    };
+	:
+	Application(props),
+	m_StartupLayer(Shared<StartupLayer>::Create())
+{
+	const auto wantProjectSelectorFn = [this] {
+		Run::Later([=] {
+			PopLayer();
+			m_PreLoader->Reset();
+			m_EditorLayer.Reset();
+			PushLayer(m_StartupLayer);
+				   });
+	};
+	const auto projectSelectFn = [wantProjectSelectorFn, this](const Shared<Project> &project) {
+		Run::Later([=] {
+			PopLayer();
+			m_PreLoader->Reset();
+			m_EditorLayer = Shared<EditorLayer>::Create(project);
+			m_EditorLayer->GetSignal(EditorLayer::Signals::OnWantProjectSelector).Connect(wantProjectSelectorFn);
+			PushLayer(m_EditorLayer);
+				   });
+	};
 
-    m_StartupLayer->GetSignal(StartupLayer::Signals::OnProjectSelect).Connect(projectSelectFn);
+	m_StartupLayer->GetSignal(StartupLayer::Signals::OnProjectSelect).Connect(projectSelectFn);
 
 
-    // TEMP
-    auto project = Shared<Project>::Create("C:/Users/ownem/source/repos/SaffronCmake/Games/2DGameProject/2DGameProject.spr");
-    m_EditorLayer = Shared<EditorLayer>::Create(project);
-    // ----
+	// TEMP
+	auto project = Shared<Project>::Create("C:/Users/ownem/source/repos/SaffronEngine/Games/2DGameProject/2DGameProject.spr");
+	m_EditorLayer = Shared<EditorLayer>::Create(project);
+	// ----
 }
 
-void EditorApplication::OnInit() {
-    PushLayer(m_EditorLayer);
+void EditorApplication::OnInit()
+{
+	PushLayer(m_EditorLayer);
 }
 
-void EditorApplication::OnUpdate() {
+void EditorApplication::OnUpdate()
+{
 }
 
 }
