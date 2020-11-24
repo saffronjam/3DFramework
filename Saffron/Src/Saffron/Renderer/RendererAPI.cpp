86f464920d04e398bd9a48d7dd6d42efed947fa3
Emil Karlsson <own.emka@gmail.com>
2020-11-24 11:24:33 +0100

    Temporary solution to failed case sensitive

--- Saffron/Src/Saffron/Renderer/RendererAPI.cpp
+++
@@ -1,9 +0,0 @@
-#include "SaffronPCH.h"
-
-#include "Saffron/Renderer/RendererAPI.h"
-
-namespace Se
-{
-RendererAPI::Capabilities RendererAPI::m_sCapabilities = {};
-float RendererAPI::m_LineThickness = 1.0f;
-}
